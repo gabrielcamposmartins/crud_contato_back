f1515b58001a39135b07fb4f4e5eed3d1aa2295d
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-02-02 17:48:21 -0300

    criação do projeto

---
+++ src/main/java/crudcontatos/CrudContatosApplication.java
@@ -0,0 +1,13 @@
+package crudcontatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crudcontatos/CrudContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package crudcontatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
