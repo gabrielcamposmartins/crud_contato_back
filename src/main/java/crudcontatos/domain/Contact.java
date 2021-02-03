4abbb0a3360da435a95494c220abaa48fd7e81af
Gabriel Campos <gabrielcamposmartins99@gmail.com>
2021-02-02 22:05:40 -0300

    mudança no nome da tebela de contact pra contacts

--- src/main/java/crudcontatos/domain/Contact.java
+++ src/main/java/crudcontatos/domain/Contact.java
@@ -6,8 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "contacts")
 public class Contact implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
